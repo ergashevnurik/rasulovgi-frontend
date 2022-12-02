844c704e6a27acef24b7b1ae5ec32d1925edecd9
Jorabek <jyoqubov36@gmail.com>
2022-12-02 15:52:10 +0500

    Warehouse

--- src/views/warehouse/warehouse-list.component.js
+++ src/views/warehouse/warehouse-list.component.js
@@ -155,7 +155,7 @@ export default class WarehousesList extends Component {
     } = this.state;
 
     return (
-      <section className="warehouse-list-section">
+      <section className="warehouse-list-section aside-container-panel">
         <div className="">
           <div className="row m-0 p-0">
             <div className="col-md-2 aside-container-panel m-0 p-0">
