d179bf17690f40951edb30fca33e8d109c573d48
Behzod <behzodsharifjonov048@gmail.com>
2022-12-03 03:53:42 -0800

    Some changes by Behzod

--- src/App.js
+++ src/App.js
@@ -22,16 +22,19 @@ import AddUser from "./views/user/add-user.component";
 import UsersList from "./views/user/user-list.component";
 import User from "./views/user/user.component";
 import Navbar1 from "./components/navbar/navbar1";
+import Footer from "./components/footer/footer";
+
 
 class App extends Component {
   
 
   render() {
-    
 
     return (
-      <div>
-        <div>
+
+      <div className="page-container">
+        <div className="content-wrap">
+
           <Routes>
             <Route path="/" element={<Login />} />
             
@@ -54,6 +57,8 @@ class App extends Component {
             <Route path="/mod" element={<BoardModerator />} />
             <Route path="/admin" element={<BoardAdmin />} />
           </Routes>
+
+          <Footer />
         </div>
 
         {/* <AuthVerify logOut={this.logOut}/> */}
