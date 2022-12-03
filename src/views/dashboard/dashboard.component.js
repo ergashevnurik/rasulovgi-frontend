ba54175d380ea8463d934e69fd432bce8478d662
Nurmukhammad Ergashev <ergashev.nurik01@mail.ru>
2022-12-03 12:59:12 +0500

    Some changes

--- src/views/dashboard/dashboard.component.js
+++ src/views/dashboard/dashboard.component.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
 import Navbar from '../../components/navbar/navbar';
 import Sidebar from "../../components/sidebar/index";
+import AuthService from "../../services/auth.service";
+import { Navigate } from "react-router-dom";
 
 export default class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: {username: ''}
+    }
+  }
+
+  componentDidMount() {
+    const currentUser = AuthService.getCurrentUser();
+
+    if (!currentUser) this.setState({ redirect: "/" });
+    this.setState({ currentUser: currentUser, userReady: true })
+
+  }
+
   render() {
+
+    if (this.state.redirect) {
+      return <Navigate to={this.state.redirect} />
+    }
+
+    const { currentUser } = this.state;
+
     return (
         <section className="dashboard-section">
         <div className="">
